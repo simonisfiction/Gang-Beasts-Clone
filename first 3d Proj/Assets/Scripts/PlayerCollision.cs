2b238b82ae4408ebebb4a736a46f761374fce84e
simonisfiction <59346539+simonisfiction@users.noreply.github.com>
2020-05-27 22:09:40 -0700

    implement player collision with lava
    debug.log("you hit lava, you dies");

---
+++ first 3d Proj/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+       
+        if (collisionInfo.collider.tag == "Deadly Surface")
+        {
+            Debug.Log("You hit lava, and you died");
+        }
+    }
+}
